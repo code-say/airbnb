3a7b891d1f462b154757dc9c72ae0916b2f8f221
sayan <sayanmandal8643@gmail.com>
2021-11-12 13:06:26 +0530

    'build'

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com'],
     },
+    env:{
+        mapbox_key: 'pk.eyJ1IjoiY29kZXNheSIsImEiOiJja3Z0NXBjODgwamxzMm5rM3hkNzY5eHFnIn0.V-EVMOQ7xKiz1D2GxJwB-g'
+    }
 };
